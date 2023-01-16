06a46e8e4e7ff68be30565505954c35b748c1417
Matthew Chick <c.chick@unsw.edu.au>
2023-01-16 14:00:39 +1100

    Install npm i dotenv, create .gitignore and .env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";  
 // has an error cannot use import statement outside a module
 // add type = modules in package.json to fix the above error
-
 // const express = require("express");
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.get("/users", (req, res) => {  // Use an arrow => instead of function() in ES6
@@ -12,6 +13,8 @@ app.get("/users", (req, res) => {  // Use an arrow => instead of function() in E
     });
 });
 
-app.listen(8000, () => {
-    console.log('Node server is running on port 8000');
+const port = process.env.PORT || 8000;  // Get env variable from .env
+
+app.listen(port, () => {
+    console.log(`Node server is running on port ${port}`);
 });
