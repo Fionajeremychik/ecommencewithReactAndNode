27d98c894acfbbd26981f9e133d2913ddcb6bdd7
Matthew Chick <c.chick@unsw.edu.au>
2023-01-16 13:47:57 +1100

    Use arrow => instead of function in ES6

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require("express");
+import express from "express";  
+// has an error cannot use import statement outside a module
+// add type = modules in package.json to fix the above error
+
+// const express = require("express");
 
 const app = express();
 
-app.get("/users", function(req, res) {
+app.get("/users", (req, res) => {  // Use an arrow => instead of function() in ES6
     res.json({
         data: 'Matthew',
     });
 });
 
-app.listen(8000, function() {
+app.listen(8000, () => {
     console.log('Node server is running on port 8000');
 });
