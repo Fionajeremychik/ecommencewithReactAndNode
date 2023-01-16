854c3ae9534367742b12e1c8b93ddbd8262cf07b
Matthew Chick <c.chick@unsw.edu.au>
2023-01-16 14:28:54 +1100

    npm i mongoose and connect mongoose db

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import express from "express";
 // add type = modules in package.json to fix the above error
 // const express = require("express");
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
 
+// db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
+  
 app.get("/users", (req, res) => {  // Use an arrow => instead of function() in ES6
     res.json({
         data: 'Matthew',
