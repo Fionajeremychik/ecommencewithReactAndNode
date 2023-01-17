f13fd9411ac64a9bc426e9d78846568579e87348
Matthew Chick <c.chick@unsw.edu.au>
2023-01-17 15:42:57 +1100

    have a problem, 404 to send request from Postman to node

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,3 +6,50 @@ export const users = async (req, res) => {
         data: 'Matthew under a router',
     });
 };
+
+export const register = async (req, res) => {
+    console.log (req.body);
+    try {
+      // 1. destructure name, email, password from req.body in ES6 (destructuring)
+      const { name, email, password } = req.body;
+      // 2. all fields require validation
+      if (!name.trim()) {
+        return res.json({ error: "Name is required" });
+      }
+      if (!email) {
+        return res.json({ error: "Email is taken" });
+      }
+      if (!password || password.length < 6) {
+        return res.json({ error: "Password must be at least 6 characters long" });
+      }
+      // 3. check if email is taken
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.json({ error: "Email is taken" });
+      }
+      // 4. hash password
+      const hashedPassword = await hashPassword(password);
+      // 5. register user
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+      // 6. create signed jwt
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      // 7. send response
+      res.json({
+        user: {
+          name: user.name,
+          email: user.email,
+          role: user.role,
+          address: user.address,
+        },
+        token,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 //controllers
-import { users } from "../controllers/auth.js";
+import { users, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
 //separate the different routes into different files and also to use router.use() to apply middleware to a specific set of routes.
 //router.route('/path').get(function)
 router.get("/users", users);  // users function is coming from controllers
+router.get("/register", register); 
 
 export default router;
