9d53fea9b1411252c77b3d277ab80dca69028abd
Matthew Chick <c.chick@unsw.edu.au>
2023-01-17 15:12:08 +1100

    npm i bcrypt and create helper file, auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 
+// Asynchronous I/O is a form of input/output processing that 
+// permits other processing to continue before the transmission has finished.
 export const users = async (req, res) => {
     res.json({
         data: 'Matthew under a router',
