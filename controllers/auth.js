cb5d843316bff0f003f612386fed52cdc624df4b
Matthew Chick <c.chick@unsw.edu.au>
2023-01-17 16:24:20 +1100

    how to register user with validation and error responses

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+import User from "../models/user.js";
+import { hashPassword, comparePassword } from "../helpers/auth.js";
 
 // Asynchronous I/O is a form of input/output processing that 
 // permits other processing to continue before the transmission has finished.
@@ -10,46 +12,47 @@ export const users = async (req, res) => {
 export const register = async (req, res) => {
     console.log (req.body);
     try {
-      // 1. destructure name, email, password from req.body in ES6 (destructuring)
-      const { name, email, password } = req.body;
-      // 2. all fields require validation
-      if (!name.trim()) {
-        return res.json({ error: "Name is required" });
-      }
-      if (!email) {
-        return res.json({ error: "Email is taken" });
-      }
-      if (!password || password.length < 6) {
-        return res.json({ error: "Password must be at least 6 characters long" });
-      }
-      // 3. check if email is taken
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.json({ error: "Email is taken" });
-      }
-      // 4. hash password
-      const hashedPassword = await hashPassword(password);
-      // 5. register user
-      const user = await new User({
-        name,
-        email,
-        password: hashedPassword,
-      }).save();
-      // 6. create signed jwt
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-      });
-      // 7. send response
-      res.json({
-        user: {
-          name: user.name,
-          email: user.email,
-          role: user.role,
-          address: user.address,
-        },
-        token,
-      });
+        // 1. destructure name, email, password from req.body
+        const { name, email, password } = req.body;
+        // 2. all fields require validation
+        if (!name.trim()) {
+            return res.json({ error: "Name is required" });
+        }
+        if (!email) {
+            return res.json({ error: "Email is taken" });
+        }
+        if (!password || password.length < 6) {
+            return res.json({ error: "Password must be at least 6 characters long" });
+        }
+        // 3. check if email is taken
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.json({ error: "Email is taken" });
+        }
+        // 4. hash password
+        const hashedPassword = await hashPassword(password);
+        // 5. register user
+        const user = await new User({
+            // name: name because the same name
+            name,
+            email,
+            password: hashedPassword,
+          }).save();
+          // 6. create signed jwt
+          //const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            //expiresIn: "7d",
+          //});
+          // 7. send response
+          res.json({
+            user: {
+              name: user.name,
+              email: user.email,
+              role: user.role,
+              address: user.address,
+            },
+            //token,
+        });
     } catch (err) {
-      console.log(err);
+        console.log(err);
     }
 };
