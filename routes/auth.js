316c85be79e4de96a00fe5810eb0c1f4fcc0f6b2
Matthew Chick <c.chick@unsw.edu.au>
2023-01-18 14:51:04 +1100

    Create Category Model

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ router.get("/users", users);  // users function is coming from controllers
 router.post("/register", register);
 router.post("/login", login);
 
-// testing with token - add requireSignin as middleware
-router.get("/secret", requireSignin, secret);
+// testing with token - add requireSignin, isAdmin as middleware
+router.get("/secret", requireSignin, isAdmin, secret);
 
 export default router;
